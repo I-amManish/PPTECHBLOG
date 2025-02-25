7a12b15aeb5100e5a86a3c97470e79d06f3af8af
Manish <manishkumaryt03@gmail.com>
2025-02-25 21:28:31 +0530

    Add authentication components and refactor existing ones

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from "./Header/Header";
-import Footer from './Footer/Footer'
+import Footer from './Footer/Footer.jsx'
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
