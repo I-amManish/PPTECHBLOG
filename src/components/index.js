b58466358dc24e59f2d0a8983c2f609619ff4466
Manish <manishkumaryt03@gmail.com>
2025-02-26 22:38:56 +0530

    new file:   src/components/Pages/AddPost.jsx 	new file:   src/components/Pages/AllPosts.jsx 	new file:   src/components/Pages/EditPost.jsx 	new file:   src/components/Pages/Home.jsx 	new file:   src/components/Pages/Login.jsx 	new file:   src/components/Pages/Post.jsx 	new file:   src/components/Pages/Signup.jsx 	modified:   src/components/index.js 	modified:   src/main.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,25 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import Post from './Post'
+import PostFrom from './PostForm'
+import PostCard from './PostCard.jsx'
+import AuthLayout from "./AuthLayout.jsx";
+
+
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostFrom,
+    PostCard,
+    AuthLayout
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,75 @@ import { Provider } from 'react-redux'
 import store from './Store/Store.js'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './components/Pages/Home.jsx'
+import AuthLayout from './components/AuthLayout.jsx'
+import Login from './components/Login.jsx'
+import SignUp from './components/Signup.jsx'
+import { Post, Signup } from './components/index.js'
+import AllPosts from './components/Pages/AllPosts.jsx'
+import EditPost from './components/Pages/EditPost.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/login',
+        element:(
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/signup',
+        element:(
+          <AuthLayout authentication={false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/allposts',
+        element:(
+          <AuthLayout authentication>
+            {""}
+            <AllPosts/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/edit-post/:slug',
+        element:(
+          <AuthLayout authentication>
+            {""}
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:'/post/:slug',
+        element:(
+          <AuthLayout authentication>
+            <Post/>
+          </AuthLayout>
+        )
+      },
+      ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
